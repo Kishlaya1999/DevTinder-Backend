d8eba3682df874811ccb60941f275feaf7c02a8e
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2025-05-24 09:32:38 +0000

    Installed nodemon as dev dependancy

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user", (req, res) => {
+  res.send("hello user");
+})
+
 app.listen(3000, () => {
   console.log("Server is sucessfully listning on PORT  3000");
 });
