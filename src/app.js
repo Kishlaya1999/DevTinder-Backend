13bfcdcfa28d8c5b5f469ff2e3fc609be38d4b34
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2025-05-24 09:38:29 +0000

    Multiple route handle example 2

--- src/app.js
+++ src/app.js
@@ -4,15 +4,18 @@ const app = express();
 
 app.use("/user", (req, res) => {
   console.log("Response 1");
-  res.send("Hello user 1");
+  // res.send("Hello user 1");
 }, (req, res) => {
   console.log("Response 2");
   res.send("Hello user 2");
 })
 
-// Output : 
-// localhost:3000/user
-// Hello User 1
+/** 
+ * Output
+ * localhost:3000/user
+ * No response comes from the server 
+ * (it seems like the response would be Hello user 2, but we will not get any response)
+ */
 
 app.listen(3000, () => {
   console.log("Server is sucessfully listning on PORT  3000");
