6e7f074da736a46cc795c20b7df3fcc208709fb1
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2025-05-24 09:35:48 +0000

    Multiple route handler example 1

--- src/app.js
+++ src/app.js
@@ -3,8 +3,16 @@ const express = require("express");
 const app = express();
 
 app.use("/user", (req, res) => {
-  res.send("hello user");
+  console.log("Response 1");
+  res.send("Hello user 1");
+}, (req, res) => {
+  console.log("Response 2");
+  res.send("Hello user 2");
 })
+
+// Output : 
+// localhost:3000/user
+// Hello User 1
 
 app.listen(3000, () => {
   console.log("Server is sucessfully listning on PORT  3000");
