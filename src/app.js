c5cbf3308bccb3bc620b7f817d23f39e1e085a03
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2025-05-24 10:02:23 +0000

    Multiple request example 3: variation

--- src/app.js
+++ src/app.js
@@ -4,18 +4,28 @@ const app = express();
 
 app.use("/user", (req, res, next) => {
   console.log("Response 1");
-  // res.send("Hello user 1");
+  res.send("Hello user 1");
   next();
 }, (req, res) => {
   console.log("Response 2");
-  res.send("Hello user 2");
+  // res.send("Hello user 2");
 })
 
 /** 
- * Output
+ * Output:
  * localhost:3000/user
- * Hello User 2
- * (in express the third argument in route handler is next, if we call next in the route handler then route handler next to it will be executed)
+ * Hello User 1
+ * 
+ * 
+ * Console:
+ * Response 1
+ * Response 2
+ * Error .........
+ * .....
+ * 
+ * In this case we are getting an error as "Cannot set headers after they are sent to the client"
+ * We are getting error because we are trying to change the response . A TCP conection is made client sends in which client sends the request and server sends the response for it then connection is close . 
+ * Here server is trying to send the response after connection is closed
  */
 
 app.listen(3000, () => {
