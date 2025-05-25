427922fb1ef51bbdbc3dd6fbc1d225dcd54b87fe
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2025-05-25 07:40:30 +0000

    Error handling in node

--- src/app.js
+++ src/app.js
@@ -2,32 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res, next) => {
-  console.log("Response 1");
-  next();
-  res.send("Hello user 1");
-}, (req, res) => {
-  console.log("Response 2");
-  // res.send("Hello user 2");
+app.get("/user", (req, res, next) => {
+ throw new Error("abc xyz");
 })
 
-/** 
- * Output:
- * localhost:3000/user
- * Hello User 1
- * 
- * 
- * Console:
- * Response 1
- * Response 2
- * Error .........
- * .....
- * 
- * In this case we are getting an error as "Cannot set headers after they are sent to the client"
- * We are getting error because we are trying to change the response . A TCP conection is made client sends in which client sends the request and server sends the response for it then connection is close . 
- * Here server is trying to send the response after connection is closed.
- * 
- * When express see the next() then it executes the 2nd route handler and fter the exuction of 2nd route handler it tries to executes req.send() which is prseent in 1st route handler (same reason)
+app.use("/",(err, req, res, next) => {
+  if(err) {
+    res.status(500).send("something went wrong");
+  }
+} )
+
+/**
+ * Error Handling: 
+ * if we get some error then the app.use("/") will handle it
+ * handler function basics 
+ *  if we pass 
+ *  - 2 arguments: 1st => requset, 2nd => response
+ *  - 3 arguments: 1st => requset, 2nd => response, 3rd => next
+ *  - 4 arguments: 1st => error, 2nd => request, 3rd => response, 4th => next
  */
 
 app.listen(3000, () => {
