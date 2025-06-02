255d4ae426d2884d15048562e6f401e4830cf4ac
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2025-06-02 06:46:49 +0000

    using express routers to group all the similar APIs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-const User = require("./models/user");
-const { validateSignUpData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const {userAuth} = require("./middleware/auth");
+
+const authRouter = require("./routers/auth");
+const profileRouter = require("./routers/profile");
+const requestRouter = require("./routers/request");
 
 // express.json is a middleware provided by express for converting the incomming body from request in appropriate format
 // express.json() => converts JSON body --> JS object
@@ -15,146 +14,16 @@ app.use(express.json());
 // Middleware for reading incoming cookies form the client
 app.use(cookieParser());
 
-app.post("/signup", async (req, res) => {
 
-  try {
-    // Validation for data;
-    validateSignUpData(req);
-
-    // Password encryption
-    const {firstName, lastName, emailId, password} = req.body;
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-    })
-    await user.save();
-    res.send("User Saved Successfully!!")
-  } catch (error) {
-    res.status(500).send(error.message)
-  }
-
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const {emailId, password} = req.body;
-
-    const user = await User.findOne({emailId: emailId});
-    
-    if(!user) {
-      throw new Error("Invalid Credentials");
-    }
-
-    // Using Schema method for some user specific task
-    const isPasswordValid = await user.validatePassword(password);
-
-    if(isPasswordValid) {
-
-      const token = await user.getJWToken();
-      
-      // Add the token to the cookie and send the response back to the user
-      res.cookie("token", token);  
-
-      res.send("User logged in sucessfully!!");
-    } else{
-      throw new Error("Invalid Credentials");
-    };
-
-  } catch (error) {
-    res.status(500).send(error.message)
-  }
-});
-
-app.get("/profile", userAuth, async (req, res) => {
-  try {
-
-    res.send(req?.user);
-    
-  } catch (error) {
-    res.status(500).send(error.message)
-  }
-})
-
-app.get("/user", async (req, res) => {
-  const { emailId } = req.body;
-  console.log(emailId);
-
-  try {
-    const user = await User.findOne({ emailId: emailId });
-
-    if (user.length === 0) {
-      res.status(404).send("User Not found");
-    } else {
-      res.send(user);
-    }
-  } catch (error) {
-    res.status(500).send("Something went wrong");
-  }
-})
-
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).send(users);
-  } catch (error) {
-    req.status(500).send("Can't get users!! Something went wrong");
-  }
-});
-
-// Detele a user from the database
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    const user = await User.findByIdAndDelete({ _id: userId });
-    //const user = await User.findByIdAndDelete(userId);
-
-    res.send("User deleted successfully");
-  } catch (err) {
-    res.status(400).send("Something went wrong ");
-  }
-});
-
-// Update data of the user
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    // Only fieldNames which are there in ALLOWED_UPDATES can be updated if someother field is inclued in req.body then error would be thrown Update not allowed
-    // By doing this user won't be able to update field like emailId, firstName, etc.....  
-    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed");
-    }
-    if (data?.skills.length > 10) {
-      throw new Error("Skills cannot be more than 10");
-    }
-    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    console.log(user);
-    res.send("User updated successfully");
-  } catch (err) {
-    res.status(400).send("UPDATE FAILED:" + err.message);
-  }
-});
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 app.use("/", (err, req, res, next) => {
   if (err) {
     res.status(500).send("Route not defined");
   }
-})
-
-
+});
 
 connectDB().then(() => {
   console.log("Database connection established...");
