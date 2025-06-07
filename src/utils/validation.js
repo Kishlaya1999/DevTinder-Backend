17995e0336ca484ab5274169a5924f3e546e581d
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2025-06-07 12:28:35 +0000

    Profile update and password reset APIs created

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,21 +1,47 @@
 const validator = require("validator");
 
 const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid!");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid!");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong Password!");
+  }
+};
 
-    const {firstName, lastName, emailId, password} = req.body;
+const validateEditProfileData = (req) => {
 
-    if(!validator.isEmail(emailId)) {
-        throw new Error("Email is not valid !!");
-    } 
+    /**
+     * This function checks if the user is trying to change any field 
+     * that should not be allowed to change later (such as email).
+     * It also checks if the user is trying to send any invalid fields.
+     */
 
-    if(!validator.isStrongPassword(password)) {
-        throw new Error("Password needs to contain at least 8 characters which should include 1 uppercase, 1 lowercase, 1 symbol and 1 number");
-    }
+	const allowedEditFields = [
+		"firstName",
+		"lastName",
+		"photoUrl",
+		"gender",
+		"age",
+		"about",
+		"skills",
+	];
+	const isEditAllowed = Object.keys(req.body).every((field) =>
+		allowedEditFields.includes(field)
+	);
 
-    /* Other validations..............*/
+    return isEditAllowed; // Returns true if all fields are allowed, false otherwise
+};
 
+const validatePasswordStrenght = (req) => {
+	const { updatedPassword } = req.body;
+	return validator.isStrongPassword(updatedPassword);
 }
 
 module.exports = {
-    validateSignUpData
+	validateSignUpData,
+	validateEditProfileData,
+	validatePasswordStrenght,
 };
