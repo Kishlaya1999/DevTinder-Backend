2899c1f878ac886ab9e0ae6a663ecc8d373514af
Kishlaya1999 <kishlayacs9935996006@gmail.com>
2025-08-15 12:57:49 +0530

    Chatting functionality completed on UI

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,9 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (loggedInUserId, targetUserId) => {
+    return crypto.createHash('sha256').update([loggedInUserId, targetUserId].sort().join("@")).digest('hex');
+}
 
 const initializeSocket = (server) => {
     const io = socket(server, {
@@ -10,9 +15,20 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
     //   handle events
 
-        socket.on("joinChat", () => {});
+        socket.on("joinChat", ({firstName ,loggedInUserId, targetUserId}) => {
+            // For making the room ID unique, we can sort the user IDs and join the room with a combined string
+            const roomId = getSecretRoomId(loggedInUserId, targetUserId);
+            console.log(`${firstName} User ${loggedInUserId} joined chat with ${targetUserId} : ${roomId}`);
+            // loggedInUserId and targetUserId are joining the same room
+            socket.join(roomId)
+        });
 
-        socket.on("sendMessage", () => {});
+        //Catching the message from the client and broadcasting it to the room according to the roomId
+        socket.on("sendMessage", ({firstName, loggedInUserId, targetUserId, text}) => {
+            const roomId = getSecretRoomId(loggedInUserId, targetUserId);
+            console.log(firstName + " " + text);
+            io.to(roomId).emit("messageReceived", {firstName, text, loggedInUserId, targetUserId});
+        });
 
         socket.on("disconnect", () => {});
     })
